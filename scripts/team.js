dd86abb9ea50972b0b166b8342ec59432c55c4fc
Alex Petrosh <petrosh@users.noreply.github.com>
2016-06-16 15:13:28 +0200

    Show default team if no hash is provided

--- scripts/team.js
+++ scripts/team.js
@@ -2,20 +2,19 @@
 
 var teamLoad = repoOwner;
 
-if (urlHash && urlHash.slice(0,5) === 'team=') {
-  teamLoad = urlHash.slice(5);
-  apiCall({
-    url: fileUrl('teams', 'teams/' + teamLoad + '.json'),
-    cb: function() {
-      jsonTeam = JSON.parse(b64d(this.content));
-      shaTeam = this.sha;
-      showTeam();
-    },
-    err: function() {
-      domAlert('team ' + teamLoad + ': not found');
-    }
-  });
-}
+if (urlHash && urlHash.slice(0,5) === 'team=') teamLoad = urlHash.slice(5);
+
+apiCall({
+  url: fileUrl('teams', 'teams/' + teamLoad + '.json'),
+  cb: function() {
+    jsonTeam = JSON.parse(b64d(this.content));
+    shaTeam = this.sha;
+    showTeam();
+  },
+  err: function() {
+    domAlert('team ' + teamLoad + ': not found');
+  }
+});
 
 function showTeam(){
   domAppend({tag: 'li', parent: domUlGame, innerHTML: 'team loaded: ' + teamLoad});
